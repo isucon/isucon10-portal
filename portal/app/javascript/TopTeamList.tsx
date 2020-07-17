80fee6574d0d791f3bb515066a7cf4d58e4cc19c
progfay <shun1856shun@gmail.com>
2020-07-17 11:55:05 +0900

    feat(TopTeamList): set tabs for show individual ranking

--- portal/app/javascript/TopTeamList.tsx
+++ portal/app/javascript/TopTeamList.tsx
@@ -1,8 +1,8 @@
-import React from "react"
+import React, { useState } from "react"
 
 import type { isuxportal } from "./pb"
 
-type Team = Pick<isuxportal.proto.services.audience.ListTeamsResponse.ITeamListItem, 'name'>
+type Team = Pick<isuxportal.proto.services.audience.ListTeamsResponse.ITeamListItem, 'name' | 'isStudent'>
 
 interface TeamItemProps {
     rank: number
@@ -12,48 +12,92 @@ interface TeamItemProps {
 const TeamItem: React.FC<TeamItemProps> = ({ rank, team }) => (
     <tr>
         <th>{rank}</th>
-        <td>{team.name}</td>
+        <td>
+            {team.name}
+        </td>
+        <td>
+            {team.isStudent && <span className="tag is-primary is-pulled-right">Student</span>}
+        </td>
     </tr>
 )
 
 const teams: Team[] = [
-    { name: '超すごいチーム' },
-    { name: '結構すごいチーム' },
-    { name: 'だいぶすごいチーム' },
-    { name: 'すごいチーム' },
-    { name: 'ちょっとすごいチーム' },
-    { name: '少しすごいチーム' },
-    { name: 'まあ普通なチーム' },
-    { name: 'そこそこ普通なチーム' },
-    { name: '普通なチーム' },
-    { name: '普通なチーム' },
-    { name: '普通なチーム' },
-    { name: '普通なチーム' },
-    { name: '普通なチーム' },
-    { name: '普通なチーム' },
-    { name: '普通なチーム' },
-    { name: '普通なチーム' },
-    { name: '普通なチーム' },
-    { name: '普通なチーム' },
-    { name: '普通なチーム' },
-    { name: '普通なチーム' },
-    { name: '普通なチーム' },
+    { name: '超すごいチーム', isStudent: false },
+    { name: '結構すごいチーム', isStudent: true },
+    { name: 'だいぶすごいチーム', isStudent: false },
+    { name: 'すごいチーム', isStudent: false },
+    { name: 'ちょっとすごいチーム', isStudent: true },
+    { name: '少しすごいチーム', isStudent: false },
+    { name: 'まあ普通なチーム', isStudent: false },
+    { name: 'そこそこ普通なチーム', isStudent: true },
+    { name: '普通なチーム', isStudent: true },
+    { name: '普通なチーム', isStudent: false },
+    { name: '普通なチーム', isStudent: false },
+    { name: '普通なチーム', isStudent: false },
+    { name: '普通なチーム', isStudent: false },
+    { name: '普通なチーム', isStudent: false },
+    { name: '普通なチーム', isStudent: true },
+    { name: '普通なチーム', isStudent: false },
+    { name: '普通なチーム', isStudent: false },
+    { name: '普通なチーム', isStudent: false },
+    { name: '普通なチーム', isStudent: false },
+    { name: '普通なチーム', isStudent: true },
+    { name: '普通なチーム', isStudent: false },
 ]
 
+type Mode = 'all' | 'general' | 'students'
+
 export const TopTeamList = () => {
+    const [mode, setMode] = useState<Mode>('all')
+
     return (
-        <table className="table is-bordered is-hoverable is-fullwidth">
-            <thead>
-                <tr className="has-background-light">
-                    <th>Rank</th>
-                    <th>Team</th>
-                </tr>
-            </thead>
-            <tbody>
-                {teams.map((team, rank) => (
-                    <TeamItem team={team} rank={rank} key={rank} />
-                ))}
-            </tbody>
-        </table>
+        <>
+            <div className="tabs is-boxed mb-0">
+                <ul>
+                    <li className={mode === "all" ? "is-active" : ""}>
+                        <a onClick={() => setMode('all')}>
+                            <span>All</span>
+                        </a>
+                    </li>
+                    <li className={mode === "general" ? "is-active" : ""}>
+                        <a onClick={() => setMode('general')}>
+                            <span>General</span>
+                        </a>
+                    </li>
+                    <li className={mode === "students" ? "is-active" : ""}>
+                        <a onClick={() => setMode('students')}>
+                            <span>Students</span>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+            <table className="table is-hoverable is-fullwidth">
+                <thead>
+                    <tr className="has-background-light">
+                        <th>Rank</th>
+                        <th>Team</th>
+                        <th>{/* isStudent? */}</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {teams.filter(
+                        ({ isStudent }) => {
+                            switch (mode) {
+                                case 'all':
+                                    return true
+                                case 'general':
+                                    return !isStudent
+                                case 'students':
+                                    return isStudent
+                                default:
+                                    true
+                            }
+                        }
+                    ).map((team, rank) => (
+                        <TeamItem team={team} rank={rank} key={rank} />
+                    ))}
+                </tbody>
+            </table>
+        </>
     )
 }
